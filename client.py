5dba32519eae05e52b396a010362aeb827de6981
isigr57 <grauisaac57@gmail.com>
2020-05-01 15:47:12 +0200

    DBA ACABAT FALTA BROADCAST

--- client.py
+++ client.py
@@ -11,12 +11,10 @@ IP_address = "localhost"
 Port = 5001
 server.connect((IP_address, Port))
 trama_up={}
-id=input("ID for the new ONT or 'A' for autogenerate: ")
-if id=='A':
-    id=''.join([random.choice(string.ascii_letters + string.digits) for n in range(16)])
-bandwith=input("BANDWITH for the new ONT: ")
-priority=input("PRIORITY for the new ONT: ")
-id_OLTStandard=priority+bandwith;
+id=input("ID for the new ONT or leave blank for autogenerate: ") or ''.join([random.choice(string.ascii_letters + string.digits) for n in range(16)])
+bandwith=input("BANDWITH in MB/s for the new ONT or blank for random [0-2480]: ") or str(random.randint(0,2480))
+priority=input("PRIORITY for the new ONT or blank for randon [0-4]: ") or str(random.randint(1,4))
+id_OLTStandard=priority+bandwith+id;
 trama_up.update({id_OLTStandard : [id, bandwith, priority]})
 os.system("clear")
 print("Information: ID = "+id+" | Bandwith = "+bandwith+" | Priority = "+priority)
--- operatr.py
+++ operatr.py
@@ -5,6 +5,7 @@ import _thread
 from terminaltables import AsciiTable
 import os
 import time
+import collections
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,8 +15,7 @@ server.bind((IP_address, Port))
 server.listen(4096)
 list_of_clients = []
 table_ONTS_in={}
-table_ONTS_out=[]
-max_cap = 2480
+table_ONTS_error=[]
 
 def print_table(table):
     out=AsciiTable(table)
@@ -39,10 +39,13 @@ def serverupdatetread():
     while True:
         if bool(table_ONTS_in):
             os.system("clear")
+            print("\n\n----ALLOCATED TABLE----\n\n")
             print_table(DBA(table_ONTS_in))
-            refresh=input("Enter 'R' for refreshing: ")
+            print("\n\n----ERROR TABLE----\n\n")
+            print_table(table_ONTS_error)
+            refresh=input("Enter 'R' for refreshing output: ")
             while refresh not in ('r','R'):
-                refresh=input("Enter 'R' for refreshing: ")
+                refresh=input("Enter 'R' for refreshing output: ")
             table_ONTS_in.clear()
         else:
             os.system("clear")
@@ -67,26 +70,57 @@ def remove(connection):
 
 
 def DBA(table_in):
-    priority_con=[0,0,0,0]
-    acumband=[0,0,0,0]
+    pr=[0,0,0,0]
+    bd=[0,0,0,0]
     allocid=1000
     global max_cap
+    global table_ONTS_error
+    table_ONTS_error=[['Id ONT','Bandwith in MB/s','Priority', 'Alloc-ID', 'Assigned Bandwith']]
+    restant = max_cap
     table_out=[]
-    table_in={k: v for k, v in sorted(table_in.items(), key=lambda item: item[1])}
+    table_in=collections.OrderedDict(sorted(table_in.items()))
+    print(table_in)
     table_out.append(['Id ONT','Bandwith in MB/s','Priority', 'Alloc-ID', 'Assigned Bandwith'])
     for key in table_in.keys():
-        priority_con[int(key[0])-1]=priority_con[int(key[0])-1]+1
-        priority_con[int(key[0])-1]=priority_con[int(key[0])-1]+int(key.value[1])
+        pr[int(key[0])-1]=pr[int(key[0])-1]+1
+        bd[int(key[0])-1]=bd[int(key[0])-1]+int(table_in[key][1])
+    for i in range (0,4):
+        iter=0
+        #print("change" + str(i) +"bd: "+str(bd[i])+"  "+str(restant))
+        selectedkeys=[]
+        if restant==0:
+            factor=-1
+        elif bd[i]>restant:
+            factor = restant/bd[i]
+        else:
+            factor=1
+        for key in table_in.keys():
+            if iter==pr[i]:
+                #print("break in iter  "+str(iter))
+                break
+            if factor==-1:
+                table_ONTS_error.append([table_in[key][0],table_in[key][1],table_in[key][2],"Not Allocated",0])
+            else:
+                table_out.append([table_in[key][0],table_in[key][1],table_in[key][2],allocid,"{:.2f}".format(int(table_in[key][1])*factor)])
+                restant=restant-(int(table_in[key][1])*factor)
+            selectedkeys.append(key)
+            allocid=allocid+1
+            if restant<0:
+                restant=0
+            iter=iter+1
+        for key in selectedkeys:
+            if key in table_in:
+                del table_in[key]
+    #print(pr)
+    #print(bd)
     return table_out
 
 
-
-max_cap = input("Enter Gran Map Size [0-2480] (MB/s) or leave blank for default 2480: ")
+max_cap = int(input("Enter Gran Map Size [0-2480] (MB/s) or leave blank for default 2480: ") or "2480")
 _thread.start_new_thread(serverupdatetread,())
 while True:
     conn, addr = server.accept()
     list_of_clients.append(conn)
     _thread.start_new_thread(clientthread,(conn,addr))
-
 conn.close()
 server.close()
